b8d55b6ce75f12a23b0a9a2e80882be71b5d49bb
DehNutCase <dslayerz@hotmail.com>
2020-05-26 18:44:08 -0700

    Modified Lab 2 to follow lecture algorithm more closely

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 def read_grammar_file(file):
   #takes a file of the format specified in http://www.cse.csusb.edu/egomez/cs570/lab4.txt
   #returns a dictionary of dictionaries
@@ -36,49 +38,52 @@ def read_grammar_file(file):
   file.close()
   return grammar, start
 
-def first(x, grammar, prev, this, first_time):
+
+def first_x(x, grammar, first, first_prev):
   #computes 'first,' the set of terminals that begin strings
   #derived from x
-  #Hackish solution using first_time to allow recusively
-  #adding to first
-  #first_time has to be initialized to be larger than the number
-  #on non_terminals
-  #very inefficient solution
-  first_x = {}
-  first_time -= 1
+  #x is a single character, not a string
+  if not x in first:
+    first[x] = {}
   
   #if x is a terminal, x is the only thing in first(x)
   if (x in grammar['terminals'] or x == 'e'):
-    first_x[x] = True
-    return first_x
+    first[x][x] = True
+    return first[x]
+    
   #If we're here, x isn't a terminal
-  else:
-    if 'e' in grammar[x]:
-      first_x['e'] = True
+  if 'e' in grammar[x]:
+    first[x]['e'] = True
+  
+  #we need to repeat this step until none of the first sets in
+  #the grammar changes
+  while first != first_prev:
+    first_prev = first.copy() #this will check for changes
+    #dictionaries are mutable types, so first_prev = first would make 
+    #the two dictionaries point to the same thing
+    #make sure to use .copy()
+    
     for i in grammar[x]:
-      broken = False
-      prev = this
-      this = first_x
-      if this != prev or first_time:
-        for j in i:
-          first_j = first(j, grammar, prev, this, first_time)
-          for k in first_j:
-            #everything in first(j) is in first(x)
-            if k != 'e':
-              first_x[k] = True
-            
-          #if j can't turn into 'e' then nothing after join
-          #can be in first(x), since j blocks it
-          if not 'e' in first_j:
-           broken = True
-           break
-          #if 'e' is in everything, then 'e' is in first_x
-        if not broken:
-          first_x['e'] = True
+      #each i is a production, a production is in the form Y1Y2Y3...
+      index = 0
+      while (index < len(i)):
+        first_index = first_x(i[index], grammar, first, first_prev)
+        
+        for j in first_index:
+          first[i[index]][j] = True
+        for j in first[i[index]]:
+          if j != 'e':
+            first[x][j] = True
+        
+        if not 'e' in first[i[index]]:
+          break
+        index += 1
+        if (index >= len(i)):
+          first[x]['e'] = True
 
-    return first_x
+  return first[x]
 
-def first_string(string_x, grammar):
+def first_string(string_x, grammar, first, first_prev):
   #computes 'first,' the set of terminals that begin strings
   #derived from x, except x may be a string of grammar symbols
   #rather than a single symbol
@@ -86,22 +91,22 @@ def first_string(string_x, grammar):
   first_string_x = {}
   index = 0
   while (index < len(string_x)):
-    first_index = first(string_x[index], grammar, {}, {}, 5)
+    first_index = first_x(string_x[index], grammar, first, first_prev) #not sure if this works properly
     for i in first_index:
       if i != 'e':
         first_string_x[i] = True
         
     #stop looping if 'e' isn't part of first_index
     #if 'e' is part of everything, then 'e' is part of string_x
-    first_string_x.pop('e', False)
     if not 'e' in first_index:
       break
-    first_string_x['e'] = True
     index += 1
+    if index >= len(string_x):
+      first_string_x['e'] = True
   
   return first_string_x
 
-def follow(x, start, grammar, prev, this, first_time):
+def follow(x, start, grammar, follow_this, follow_prev):
   #computes 'follow,' the set of terminals that can appear
   #immediately to the right of x in a sentential form
   
@@ -109,93 +114,127 @@ def follow(x, start, grammar, prev, this, first_time):
   if x in grammar['terminals']:
     return False
     
-  follow_x = {}
+  if not x in follow_this:
+    follow_this[x] = {}
+    
   #'$' is our special endmarker symbol
   if x == start:
-    follow_x['$'] = True
+    follow_this[x]['$'] = True
   
   #If there's a production A -> αBβ, then everything in first(β)
   #other than 'e,' is in follow(B)
-  for i in grammar:
-    #our grammar contains terminals and non-terminals
-    #we're ignoring terminals since they dont' have productions
-    if i == 'terminals':
-      continue
+  
+  while follow_this != follow_prev:
+    follow_prev = follow_this.copy()
     
-    for j in grammar[i]:
-      #search through every production for something of the form αBβ
-      #α is allowed to be 'e'
-      index = 0
-      while (index < len(j) - 1):
+    for i in grammar:
+      #our grammar contains terminals and non-terminals
+      #we're ignoring terminals since they dont' have productions
+      if i == 'terminals':
+        continue
+      
+      for j in grammar[i]:
+        #search through every production for something of the form αBβ
+        #α is allowed to be 'e'
+        index = 0
+        while (index < len(j) - 1):
+          if j[index] == x:
+            #αBβ, everythign in first(β) is in follow(B)
+            first_b = first_string(j[index+1:], grammar, {}, {})
+            for k in first_b:
+              if k != 'e':
+                follow_this[x][k] = True
+                
+            #if 'e' is in first(β), everything in Follow(i) is in Follow(B)
+            if 'e' in first_b:
+              if not i in follow_this:
+                follow_this[i] = {}
+              for k in follow(i, start, grammar, follow_this, follow_prev):
+                follow_this[i][k] = True
+              for k in follow_this[i]:
+                follow_this[x][k] = True
+          index += 1
+          
+        #we're now at the very last symbol, if it's B then everything in follow(A)
+        #is in follow(B)
         if j[index] == x:
-          first_b = first_string(j[index+1], grammar)
-          for k in first_b:
-            if k != 'e':
-              follow_x[k] = True
-              
-          #if 'e' is in first(β), everything in Follow(i) is in Follow(B)
-          if 'e' in first_b:
-            for k in follow(i, start, grammar, prev, this, False):
-              follow_x[k] = True
-        index += 1
-        
-      #we're now at the very last symbol, if it's B then everything in follow(A)
-      #is in follow(B)
-      #we need to stop doing this once follow sets stop being added to
-      if j[index] == x:
-        prev = this
-        this = follow_x
-        if this != prev or first_time:
-          for k in follow(i, start, grammar, prev, this, False):
-            follow_x[k] = True
+          if not i in follow_this:
+            follow_this[i] = {}
+          for k in follow(i, start, grammar, follow_this, follow_prev):
+            follow_this[i][k] = True
+          for k in follow_this[i]:
+            follow_this[x][k] = True
         
 
-  return follow_x
+  return follow_this[x]
       
   
 file = open('g417', 'r')
 grammar, start = read_grammar_file(file)
+first_g417 = {}
 
 print('For file = "g417":')
-print('Printing firsts:')
-for i in grammar:
+print('Printing Productions:')
+for i in sorted(grammar):
   if i == 'terminals':
     continue
   print(i + ':')
-  for j in first(i, grammar, {}, {}, 5):
+  for j in sorted(grammar[i]):
+    print(j)
+  print('')
+
+print('Printing firsts:')
+for i in sorted(grammar):
+  if i == 'terminals':
+    continue
+  print(i + ':')
+  for j in sorted(first_x(i, grammar, {}, {})):
+    print(j, end = "")
+  print('')
+  
+print('')
+print('Printing follows:')
+for i in sorted(grammar):
+  if i == 'terminals':
+    continue
+  print(i + ':')
+  for j in sorted(follow(i, start, grammar, {}, {})):
     print(j, end = '')
   print('')
 
 print('')
-print('Printing follows:')
-for i in grammar:
-  if i == 'terminals':
-    continue
-  print(i + ':')
-  for j in follow(i, start, grammar, {}, {}, True):
-    print(j, end = '')
-  print('')
+print('')
 
 file = open('g419', 'r')
 grammar, start = read_grammar_file(file)
 
+
 print('For file = "g419":')
-print('Printing firsts:')
-for i in grammar:
+print('Printing Productions:')
+for i in sorted(grammar):
   if i == 'terminals':
     continue
   print(i + ':')
-  for j in first(i, grammar, {}, {}, 5):
-    print(j, end = '')
+  for j in sorted(grammar[i]):
+    print(j)
+  print('')
+  
+print('Printing firsts:')
+for i in sorted(grammar):
+  if i == 'terminals':
+    continue
+  print(i + ':')
+  for j in sorted(first_x(i, grammar, {}, {})):
+    print(j, end = "")
   print('')
 
 print('')
 print('Printing follows:')
-for i in grammar:
+for i in sorted(grammar):
   if i == 'terminals':
     continue
   print(i + ':')
-  for j in follow(i, start, grammar, {}, {}, True):
+  for j in sorted(follow(i, start, grammar, {}, {})):
     print(j, end = '')
   print('')
 
